f4aa74d2b9c08fd26078a3a3b5eb08ac13b800bc
shagunbhuyan29 <shagun.25bai11614@vitbhopal.ac.in>
2025-11-22 19:50:21 +0530

    Update cost_service.py

--- src/services/cost_service.py
+++ src/services/cost_service.py
@@ -1,1 +1,15 @@
+class TravelCostService:
+    def calculate_cost(self, distance, mileage, fuel_price, vehicle_type):
+        vehicle_factor = {
+            "car": 1.0,
+            "bike": 0.7,
+            "bus": 1.3
+        }
+        
+        multiplier = vehicle_factor.get(vehicle_type.lower(), 1.0)
+        
+        fuel_needed = distance / mileage
+        base_cost = fuel_needed * fuel_price
+        
+        return base_cost * multiplier
 
