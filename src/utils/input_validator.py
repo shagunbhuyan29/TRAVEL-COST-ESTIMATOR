6d4a574e99ad8a775a3bbd4eafc493041cf55caa
shagunbhuyan29 <shagun.25bai11614@vitbhopal.ac.in>
2025-11-22 20:47:43 +0530

    Update input_validator.py

--- src/utils/input_validator.py
+++ src/utils/input_validator.py
@@ -1,1 +1,18 @@
+def validate_float(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value <= 0:
+                print("Value must be positive!")
+                continue
+            return value
+        except ValueError:
+            print("Please enter a valid number!")
+
+def validate_vehicle_type():
+    while True:
+        v = input("Enter vehicle type (car/bike/truck): ").lower()
+        if v in ["car", "bike", "truck"]:
+            return v
+        print("Invalid vehicle type.")
 
